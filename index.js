7cab57b6e5ed563f501f483de5d7c0a6aedc1a4a
William Rogge <williamroggeiv@gmail.com>
2020-04-16 18:04:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-var obj = {prop: 1}
 
-function updateObjectWithKeyAndValue(object, key, value){
-  obj.prop2 = 2
-  return Object.assign({}, obj, { [key]: value})
-}
 
-obj.prop2 = 2
-
-function destructivelyUpdateObjectWithKeyAndValue (object, key, value){
-  object[key] = value
-
-  return obj
+function deleteFromObjectByKey (object, key){
+  delete obj.prop2
+  return obj  
 }
