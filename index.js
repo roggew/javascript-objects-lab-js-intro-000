1ea6f7d742a83359b112f27856491678c3181af9
William Rogge <williamroggeiv@gmail.com>
2020-04-16 19:40:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function deleteFromObjectByKey(object, key){
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
-  var obj = {prop:1, prop2:2}
+  var obj = {prop:1}
   delete obj.prop
   return obj
 }
